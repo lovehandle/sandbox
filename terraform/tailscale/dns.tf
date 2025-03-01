f7f3c5f0078877288ca787784df19d3d1cc4da20
Ryan Closner <me@ryanclosner.com>
2025-03-01 14:15:41 -0500

    feat: Set tailscale DNS to NextDNS

--- terraform/tailscale/dns.tf
+++ terraform/tailscale/dns.tf
@@ -1,4 +1,11 @@
 resource "tailscale_dns_preferences" "prefs" {
   magic_dns = true
 }
-  
+
+resource "tailscale_dns_nameservers" "ns" {
+  nameservers = ["2a07:a8c0::a4:6d2b", "2a07:a8c1::a4:6d2b"] # my nextdns
+}
+
+resource "tailscale_dns_search_paths" "sp" {
+  search_paths = []
+}
